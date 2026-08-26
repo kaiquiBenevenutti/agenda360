2d93deb945a9b3966bb66ab9860c99047616eb19
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2026-08-26 18:46:29 -0300

    initial

---
+++ src/main/java/br/com/ifsc/agenda360/Agenda360Application.java
@@ -0,0 +1,13 @@
+package br.com.ifsc.agenda360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agenda360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Agenda360Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifsc/agenda360/Agenda360ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifsc.agenda360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Agenda360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
